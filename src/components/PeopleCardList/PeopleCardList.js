f24423a39317441929a3a1be38ef48df4e01289e
marcinmadeja <madeja.m.b@gmail.com>
2018-07-07 11:50:24 +0200

    Add proptypes to PeopleCardList

--- src/components/PeopleCardList/PeopleCardList.js
+++ src/components/PeopleCardList/PeopleCardList.js
@@ -1,7 +1,15 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 const PeopleCardsList = ({ children }) => {
   return <Fragment>{children}</Fragment>;
+};
+
+PeopleCardsList.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
 };
 
 export default PeopleCardsList;
